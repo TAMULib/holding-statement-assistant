9a6024b03d3a10df3f87e48679dfba6834e022e7
Huff <jeremythuff@gmail.com>
2025-05-20 17:39:23 -0500

    Fixed things

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',            // Output folder (optional, for production builds)
+    emptyOutDir: true
+  },
+  server: {
+    // Optional: if you want to reach it from the network
+    host: true,
+    port: 5173
+  }
 });
